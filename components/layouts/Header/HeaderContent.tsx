62cfe7858494104bf5b09f30eddb921c0b0f9e27
Gustavo Leonardo <leonardomoralesgustavo@gmail.com>
2024-04-24 16:12:27 -0600

    change button text

--- components/layouts/Header/HeaderContent.tsx
+++ components/layouts/Header/HeaderContent.tsx
@@ -10,66 +10,6 @@ const { contentStyles: styles } = styleList;
 
 export const HeaderContent = () => {
   return (
-    // <Box sx={styles.root} mb={8}>
-    //   <Box sx={styles.textContainer}>
-
-    //   </Box>
-    //   <Box component={"img"} src="/images/header_img.png" sx={styles.img} bgcolor={"red"}/>
-    // </Box>
-    // <Box
-    //   sx={{
-    //     backgroundImage: `url("/images/btc_box.png")`,
-    //     backgroundSize: "cover",
-    //     backgroundPosition: "center",
-    //     width: "100%",
-    //     height: "100vh", // ajusta la altura según tus necesidades
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    // >
-    //   <Box
-    //     display={"flex"}
-    //     flexDirection={"column"}
-    //     justifyContent={"center"}
-    //     alignItems={"center"}
-    //   >
-    //     <Typography
-    //       variant="h4"
-    //       component="div"
-    //       textAlign="center"
-    //       color="white"
-    //     >
-    //       De Inversión a patrimonio,
-    //     </Typography>
-    //     <Typography
-    //       variant="h4"
-    //       component="div"
-    //       textAlign="center"
-    //       color="white"
-    //     >
-    //       Convierte Criptomonedas en Tu Propiedad
-    //     </Typography>
-    //     <Typography
-    //       variant="h4"
-    //       component="div"
-    //       textAlign="center"
-    //       color="white"
-    //     >
-    //       en Tu Propiedad
-    //     </Typography>
-    //     <Box sx={styles.buttonContainer}>
-    //       <Button
-    //         variant="contained"
-    //         onClick={() => {
-    //           window.open(`https://imperio.fitalmx.com/`);
-    //         }}
-    //       >
-    //         Simulador virtual
-    //       </Button>
-    //     </Box>
-    //   </Box>
-    // </Box>
     <>
       <Swiper
         spaceBetween={30}
@@ -124,7 +64,7 @@ export const HeaderContent = () => {
               <Button
                 variant="contained"
                 onClick={() => {
-                  window.open(`https://wallet.fitalmx.com/`);
+                  window.open(`https://imperio.fitalmx.com/`);
                 }}
               >
                 Ir a mi Wallet
@@ -251,7 +191,7 @@ export const HeaderContent = () => {
                   window.open(`https://wallet.fitalmx.com/`);
                 }}
               >
-                Ir a mi Wallet
+                Ir a mi Imperio
               </Button>
             </Box>
           </Box>
